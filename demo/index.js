043c2dc4295d925fbad6dfdacb2b1688933eb372
danielsuter <suter_daniel@hotmail.com>
2019-01-30 17:37:42 +0100

    Update example.

--- demo/index.js
+++ demo/index.js
@@ -17,6 +17,16 @@ const resolvers = {
             employees.push(newEmployee);
             return newEmployee;
         },
+        updateEmployee: (parent, {id, firstname, lastname, hobbies}) => {
+            const employeeToUpdate = employees.find(employee => "" + employee.id === id);
+            if (!employeeToUpdate) {
+                throw new Error(`Employee with id ${id} not found`);
+            }
+            employeeToUpdate.firstname = firstname || employeeToUpdate.firstname;
+            employeeToUpdate.lastname = lastname || employeeToUpdate.lastname;
+            employeeToUpdate.hobbies = hobbies || employeeToUpdate.hobbies;
+            return employeeToUpdate;
+        },
     },
     Employee: {
         fullname: (parent) => parent.firstname + ' ' + parent.lastname,
