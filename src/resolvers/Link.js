47f55e6a241170e83b8c08824973b1a18f656e6f
danielsuter <suter_daniel@hotmail.com>
2019-09-16 08:43:01 +0200

    Update dependencies and remove old tutorial code.

--- src/resolvers/Link.js
+++
@@ -1,5 +0,0 @@
-const postedBy = (parent, args, context) => context.prisma.link({id: parent.id}).postedBy();
-
-module.exports = {
-    postedBy,
-};
--- src/resolvers/Query.js
+++
@@ -1,5 +0,0 @@
-const feed = (root, args, context, info) => context.prisma.links();
-
-module.exports = {
-    feed,
-};
